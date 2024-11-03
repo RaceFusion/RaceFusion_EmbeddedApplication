9ea7902ff48d76bb197093c3bbb56fd040f63272
Giovanni Andrés Ramos Calderón <142363773+GioRC0@users.noreply.github.com>
2024-11-02 22:48:07 -0500

    feat(temperature): read temperature method

--- SensorTemperatura.cpp
+++ SensorTemperatura.cpp
@@ -6,6 +6,7 @@ void inicializarSensorTemperatura() {
 }
 
 float leerSensorTemperatura() {
+  
   int valor = analogRead(35); // Lee el valor del potenciómetro conectado a GPIO35
   return map(valor, 0, 4095, -40, 125); // Mapea el valor a un rango de -40 a 125°C (rango típico de temperatura)
 }
